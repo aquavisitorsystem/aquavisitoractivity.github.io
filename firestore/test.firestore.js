b5ab05224bcf350142bd52fae488f76a6dca87c6
Chuck Konkol <ckonkol@gmail.com>
2018-05-31 13:37:47 -0500

    Update test.firestore.js

--- firestore/test.firestore.js
+++ firestore/test.firestore.js
@@ -28,9 +28,7 @@ describe("firestore", () => {
 db.collection("users").get().then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
         // doc.data() is never undefined for query doc snapshots
-        document.write(doc.id, " => ", doc.data());
-        document.write(doc.date, " => ", doc.data());
-        document.write(doc.device, " => ", doc.data());
+        document.write(doc.id, doc.date, doc.device, doc.visited);
     });
 });
          });
