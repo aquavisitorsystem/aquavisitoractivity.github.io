b507b59e4d9363bda01986d7e4d2e41a7959a997
Chuck Konkol <ckonkol@gmail.com>
2018-06-21 13:13:05 -0500

    Update sortbyphone.js

--- firestore/sortbyphone.js
+++ firestore/sortbyphone.js
@@ -24,7 +24,7 @@ describe("aquaguide activity", () => {
       );
     });
      it("sorting by phonename asc and date desc", () => {
-   db.collection("users").orderBy("device").orderBy("date", "desc")
+   db.collection("users").orderBy("device")
     .onSnapshot(function(querySnapshot) {
         var peeps = [];
         querySnapshot.forEach(function(doc) {
