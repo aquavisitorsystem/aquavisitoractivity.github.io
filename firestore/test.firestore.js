243f2b96626b9f5f96849053393753c3449208eb
AquaDevelopers - Chuck Konkol <myaquaapps@gmail.com>
2023-10-19 08:19:32 -0500

    Update test.firestore.js

--- firestore/test.firestore.js
+++ firestore/test.firestore.js
@@ -29,7 +29,7 @@ describe("Aqua Visitor Management System activity", () => {
         var peeps = [];
         querySnapshot.forEach(function(doc) {
             var mydate = new Date(doc.data().date);
-            peeps.push("<br>" + mydate.toLocaleString() + "Visiting: " + doc.data().message + ",LName: " + doc.data().lastname + ", FName: " + doc.data().firstname + ", Company: " + doc.data().company + ", Checkin: " + doc.data().checkin + ", Checkout: " + doc.data().checkout);
+            peeps.push("<br>" + mydate.toLocaleString() + ", Visiting: " + doc.data().message + ", LName: " + doc.data().lastname + ", FName: " + doc.data().firstname + ", Company: " + doc.data().company + ", Checkin: " + doc.data().checkin + ", Checkout: " + doc.data().checkout);
         });
         document.getElementById("message").innerHTML = "Current activity: " +  "<br>" + peeps.join(" ");
        
